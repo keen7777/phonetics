405b1475a8bba2dc1415a7bc7ce09e1516e539f1
Keen <can.wang.keen@gmail.com>
2025-06-22 01:25:17 +0200

    modify requirements

--- Homepage.py
+++ Homepage.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from st_aggrid import AgGrid, GridOptionsBuilder
 
 data_de = {
     "Artikulationsart/-ort": ["Plosive", "Nasal", "Trill", "Tap or Flap","Fricative","Lateral Frikative","Approximant","Lateral Approximant"],
@@ -20,22 +19,5 @@ data_de = {
 df_de = pd.DataFrame(data_de)
 st.set_page_config(layout="wide")
 st.markdown("### IPA Consonant Chart (DE)")
-# settings
-gb_de = GridOptionsBuilder.from_dataframe(df_de)
-gb_de.configure_default_column(sortable=True, filter=True)
 
-# build and display
-gridOptions = gb_de.build()
-custom_css = {
-    ".ag-cell": {
-        "border-right": "1px solid #ccc !important",
-        "text-align": "center",
-        "vertical-align": "middle"
-    },
-    ".ag-header-cell": {
-        "border-right": "1px solid #ccc !important"
-    }
-}
-
-AgGrid(df_de, gridOptions=gridOptions,fit_columns_on_grid_load=True, custom_css=custom_css)
 
